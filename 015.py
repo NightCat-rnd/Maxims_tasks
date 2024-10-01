65c8d6f72f085cf1caaa4746bd1ca83f58a15ceb
NightCat-rnd <andrey.chekunov@gmail.com>
2024-10-01 23:24:05 +0300

    Задача 015

--- 015.py
+++ 015.py
@@ -1,5 +1,5 @@
 # найдите кол-во положительных (>0) элементов списка
-input_list = list(map(int,input('Enter list ').split()))
+input_list = list(map(int,input('Enter list: ').split()))
 sum = 0
 for i in input_list:
     if i>0:
